a91746a9613c2411739f88dad369de644e861aab
Nasser Alqutaim <nasseralqutaim@gmail.com>
2022-07-31 11:56:07 -0500

    added a mapbox which display USA map

--- src/views/StatesView.vue
+++ src/views/StatesView.vue
@@ -1,7 +1,68 @@
+<script>
+/* global mapboxgl, mapboxgl*/
+
+export default {
+  data: function () {
+    return {};
+  },
+  mounted: function () {
+    mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_KEY;
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-96, 37.8],
+      zoom: 3,
+    });
+    // map.on("mousemove", (e) => {
+    //   const features = map.queryRenderedFeatures(e.point);
+
+    //   // Limit the number of properties we're displaying for
+    //   // legibility and performance
+    //   const displayProperties = ["type", "properties", "id", "layer", "source", "sourceLayer", "state"];
+
+    //   const displayFeatures = features.map((feat) => {
+    //     const displayFeat = {};
+    //     displayProperties.forEach((prop) => {
+    //       displayFeat[prop] = feat[prop];
+    //     });
+    //     return displayFeat;
+    //   });
+
+    //   // Write object as string with an indent of two spaces.
+    //   document.getElementById("features").innerHTML = JSON.stringify(displayFeatures, null, 2);
+    // });
+
+    console.log(map);
+  },
+  methods: {},
+};
+</script>
+
+<!--  -->
+
 <template>
-  <div class="states">
-    <h1>Create interactice states map here</h1>
+  <div class="about">
+    <h1>Find each state with their accompanying data:</h1>
+    <div id="map"></div>
   </div>
 </template>
 
-<!--  -->
+<style>
+#features {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 50%;
+  overflow: auto;
+  background: rgba(255, 255, 255, 0.8);
+}
+#map canvas {
+  cursor: crosshair;
+}
+#map {
+  height: 300px;
+}
+</style>
+
+<!-- pk.eyJ1IjoiZHF1dCIsImEiOiJjbDVhMjFtbTAwN3c5M2xvN3hkMTV5cmNtIn0.LN65tHleknenlufqUSb9bw -->
